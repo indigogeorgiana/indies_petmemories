5da563c4eb6ddb9751527e39ea26b1aa35cc9d8c
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-06-19 11:00:37 +1200

    made functions for hide/show cell

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -4,14 +4,28 @@ import Cell from './Cell'
 import createBoard from '../createBoard'
 
 class Board extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       board: createBoard(4),
-      width: props.width
+      width: props.width,
+      status: 'hidden'
     }
   }
-  render() {
+
+  hideCells () {
+    this.setState({
+      status: 'hidden'
+    })
+  }
+
+  showCells () {
+    this.setState({
+      status: 'visible'
+    })
+  }
+
+  render () {
     const board = this.state.board
     const rowHeight = this.state.width / board.length
 
@@ -19,11 +33,11 @@ class Board extends React.Component {
       style={{width: this.state.width, height: this.state.width}}
       className="column board has-text-centered"
     >
-      {board.map(function(row) {
-        //render a ROW (of cells) on the Board
+      {board.map(function (row) {
+        // render a ROW (of cells) on the Board
         return <div className="row columns" style={{height: rowHeight}} >
-          {row.map(function(cell) {
-            //render each Cell within a ROW, using the Cell.jsx component
+          {row.map(function (cell) {
+            // render each Cell within a ROW, using the Cell.jsx component
             return <Cell cell={cell} />
           })}
         </div>

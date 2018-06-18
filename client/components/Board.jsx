cbad105264c8287fa81550b2daa01325d95a61ed
ZeBin Xiao <juniorxiao01@gmail.com>
2018-06-19 11:56:49 +1200

    able to hide and show buttons

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -9,25 +9,32 @@ class Board extends React.Component {
     this.state = {
       board: createBoard(4),
       width: props.width,
-      status: 'hidden'
+      visibility: 'visible'
     }
+    this.hideCells = this.hideCells.bind(this)
+    this.showCells = this.showCells.bind(this)
   }
 
   hideCells () {
     this.setState({
-      status: 'hidden'
+      visibility: 'hidden'
     })
   }
 
   showCells () {
     this.setState({
-      status: 'visible'
+      visibility: 'visible'
     })
+  }
+
+  changeVis () {
+    
   }
 
   render () {
     const board = this.state.board
     const rowHeight = this.state.width / board.length
+    const vis = this.state.visibility
 
     return <div
       style={{width: this.state.width, height: this.state.width}}
@@ -35,10 +42,12 @@ class Board extends React.Component {
     >
       {board.map(function (row) {
         // render a ROW (of cells) on the Board
-        return <div className="row columns" style={{height: rowHeight}} >
+        return <div className="row columns" style={{height: rowHeight,}} >
           {row.map(function (cell) {
             // render each Cell within a ROW, using the Cell.jsx component
-            return <Cell cell={cell} />
+            return <Cell 
+            cell={cell}
+            vis={vis} />
           })}
         </div>
       })}
--- client/components/Cell.jsx
+++ client/components/Cell.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Cell = (props) => {
-
-  return <div className="cell column">
-    {props.cell.value}
+  return <div className="cell column" >
+    <button style={{visibility: props.vis}}>
+      {props.cell.value}
+    </button>
   </div>
 }
 

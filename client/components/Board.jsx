df12dd4d6dfdb95e82eb840780930496477e7c93
Indigo Georgiana <indigogeorgiana@gmail.com>
2018-06-19 15:31:20 +1200

    Trying to complete

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -9,9 +9,7 @@ class Board extends React.Component {
     this.state = {
       board: createBoard(4),
       width: props.width,
-      height: props.height,
-      isHidden: props.isHidden
-        
+      visibility: visible        
     }
   }
 
@@ -19,7 +17,8 @@ class Board extends React.Component {
   ReactDOM.render(Parent, app)
 
     return 
-      <div style={{width: this.state.width, height: this.state.height, isHidden: this.state.isHidden}}
+      <div >
+        style={{width: this.state.width, height: this.state.height}}
       className="column board has-text-centered"
     >
   {board.map(function (row, id) {
@@ -32,4 +31,47 @@ class Board extends React.Component {
       return <Cell key={cell.value} cell={cell} onClick={this.state.cellsShow} />
    
       }
+      }import React from 'react'
+      import Cell from './Cell'
+      
+      import createBoard from '../createBoard'
+      
+      class Board extends React.Component {
+        constructor (props) {
+          super(props)
+          this.setState = {
+            board: createBoard(4),
+            width: props.width,
+            view: props.visible
+          }
+          this.isHidden = this.isHidden.bind(this)
+          this.show = this.show.bind(this)
+        }
+      
+        show (cell) {
+          console.log('showing', cell.value)
+        }
+        render () {
+          const board = this.state.board
+          const rowHeight = this.state.width / board.length
+      
+          return <div
+            style={{width: this.state.width, height: this.state.width}}
+            className="column board has-text-centered"
+          >
+            {board.map(function (row, id) {
+              // render a ROW (of cells) on the Board
+              return <div key={'row' + id}
+                className="row columns"
+                style={{height: rowHeight}} >
+                {row.map(function (cell) {
+                  // render each Cell within a ROW, using the Cell.jsx component
+                  return <Cell key={cell.value} cell={cell} isHidden={true}/>
+                })}
+              </div>
+            })}
+          </div>
+        }
       }
+      
+      export default Board
